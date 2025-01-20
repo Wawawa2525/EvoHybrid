aa9492cc5c71e2e72e8e7218ad854e46a95c7c72
Wawawa2525 <m2216034sa@ug.jwu.ac.jp>
2025-01-20 15:31:28 +0900

    Update ai.py

--- ai.py
+++ ai.py
@@ -87,33 +87,13 @@ def evaluate_board(board, stone):
 
     return score
 
-def dynamic_evaluate_board(board, stone, stage):
-    early_weight = [
-        [100, -20, 10, 10, -20, 100],
-        [-20, -50, -2, -2, -50, -20],
-        [10, -2, 1, 1, -2, 10],
-        [10, -2, 1, 1, -2, 10],
-        [-20, -50, -2, -2, -50, -20],
-        [100, -20, 10, 10, -20, 100],
-    ]
-    late_weight = [
-        [100, 80, 50, 50, 80, 100],
-        [80, 30, 20, 20, 30, 80],
-        [50, 20, 10, 10, 20, 50],
-        [50, 20, 10, 10, 20, 50],
-        [80, 30, 20, 20, 30, 80],
-        [100, 80, 50, 50, 80, 100],
-    ]
-
-    weight = early_weight if stage == "early" else late_weight
-    score = 0
-
+def get_valid_moves(board, stone):
+    valid_moves = []
     for y in range(len(board)):
         for x in range(len(board[0])):
-            if board[y][x] == stone:
-                score += weight[y][x]
-
-    return score
+            if can_place_x_y(board, stone, x, y):
+                valid_moves.append((x, y))
+    return valid_moves
 
 def apply_move(board, stone, x, y):
     new_board = [row[:] for row in board]
@@ -137,35 +117,6 @@ def apply_move(board, stone, x, y):
 
     return new_board
 
-def get_valid_moves(board, stone):
-    valid_moves = []
-    for y in range(len(board)):
-        for x in range(len(board[0])):
-            if can_place_x_y(board, stone, x, y):
-                valid_moves.append((x, y))
-    return valid_moves
-
-def can_place_x_y(board, stone, x, y):
-    if board[y][x] != 0:
-        return False
-
-    opponent = 3 - stone
-    directions = [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]
-
-    for dx, dy in directions:
-        nx, ny = x + dx, y + dy
-        found_opponent = False
-
-        while 0 <= nx < len(board[0]) and 0 <= ny < len(board) and board[ny][nx] == opponent:
-            nx += dx
-            ny += dy
-            found_opponent = True
-
-        if found_opponent and 0 <= nx < len(board[0]) and 0 <= ny < len(board) and board[ny][nx] == stone:
-            return True
-
-    return False
-
 def dynamic_evaluate_board(board, stone, stage):
     early_weight = [
         [100, -20, 10, 10, -20, 100],
@@ -173,6 +124,14 @@ def dynamic_evaluate_board(board, stone, stage):
         [10, -2, 0, 0, -2, 10],
         [10, -2, 0, 0, -2, 10],
         [-20, -50, -2, -2, -50, -20],
+        [100, -20, 10, 10, -20, 100],
+    ]
+    mid_weight = [
+        [100, -20, 10, 10, -20, 100],
+        [-20, -50, 5, 5, -50, -20],
+        [10, 5, 5, 5, 5, 10],
+        [10, 5, 5, 5, 5, 10],
+        [-20, -50, 5, 5, -50, -20],
         [100, -20, 10, 10, -20, 100],
     ]
     late_weight = [
@@ -184,9 +143,14 @@ def dynamic_evaluate_board(board, stone, stage):
         [100, 80, 50, 50, 80, 100],
     ]
 
-    weight = early_weight if stage == "early" else late_weight
-    score = 0
+    if stage == "early":
+        weight = early_weight
+    elif stage == "mid":
+        weight = mid_weight
+    else:
+        weight = late_weight
 
+    score = 0
     for y in range(len(board)):
         for x in range(len(board[0])):
             if board[y][x] == stone:
@@ -229,23 +193,13 @@ def minimax(board, stone, depth, maximizing_player, alpha=-math.inf, beta=math.i
                 break
         return min_eval
 
-def count_stable_stones(board, stone):
-    stable_count = 0
-    # 確定石ロジックを実装
-    return stable_count
-
-def evaluate_with_stable_stones(board, stone):
-    stable_stones = count_stable_stones(board, stone)
-    return dynamic_evaluate_board(board, stone, "late") + stable_stones * 50
-
-class HybridAI:
+class HybridAI(object):
 
     def face(self):
         return "✨"
 
     def place(self, board, stone):
         valid_moves = get_valid_moves(board, stone)
-
         if not valid_moves:
             return None
 
@@ -261,12 +215,14 @@ class HybridAI:
         best_score = -math.inf
         best_move = None
 
+        depth = dynamic_depth(empty_cells)
+
         for x, y in valid_moves:
             new_board = apply_move(board, stone, x, y)
             if stage == "late":
-                score = minimax(new_board, 3 - stone, depth=dynamic_depth(empty_cells), maximizing_player=False)
+                score = minimax(new_board, 3 - stone, depth, maximizing_player=False)
             else:
-                score = evaluate_with_stable_stones(new_board, stone)
+                score = dynamic_evaluate_board(new_board, stone, stage)
 
             if score > best_score:
                 best_score = score
